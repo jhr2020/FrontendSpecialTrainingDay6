53fb1431ebc01206f8f6ec0ae88e08c2ccc4976a
jhr2020 <joergmhillner@googlemail.com>
2020-10-14 15:34:46 +0000

    Day 5

--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -5,7 +5,7 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("com.cpro.jhr.training.Training1.controller.View2", {
-		
+
 		getRouter: function () {
 			return sap.ui.core.UIComponent.getRouterFor(this);
 		},
@@ -18,20 +18,31 @@ sap.ui.define([
 		onInit: function () {
 			this.getRouter().getRoute("View2").attachPatternMatched(this.onView2Matched, this);
 			this.getRouter().getRoute("View2Query").attachPatternMatched(this.onView2QueryMatched, this);
-			
+
 			this.getView().setModel(Model.createViewModel(), "viewModel");
 			var oModel = this.getView().getModel("viewModel");
 			oModel.setProperty("/bindingMode", oModel.getDefaultBindingMode());
 			oModel.setProperty("/content", "content");
+
+			oModel.setProperty("/currencyCode", "EUR");
+			sap.ui.getCore().getMessageManager().registerObject(this.getView(), true);
+			this._initMessageModel();
+			this._initMessagePopover();
+
+			oModel = new sap.ui.model.json.JSONModel();
+			oModel.loadData("localService/mockdata/products.json");
+
+			this.getView().setModel(oModel, "productsModel");
+
 		},
-		
-		onView2Matched: function(oEvent){
+
+		onView2Matched: function (oEvent) {
 			this.byId("ElementBindingForm").bindElement("/SomeEntity('someID')");
 		},
-		
-		onView2QueryMatched: function(oEvent){
+
+		onView2QueryMatched: function (oEvent) {
 			var sFluggesellschaftID = oEvent.getParameter("arguments").fluggesellschaftID;
-			this.byId("ElementBindingForm").bindElement("/flightSet('"+ sFluggesellschaftID +"')");
+			this.byId("ElementBindingForm").bindElement("/flightSet('" + sFluggesellschaftID + "')");
 		},
 
 		/**
@@ -59,19 +70,73 @@ sap.ui.define([
 		//	onExit: function() {
 		//
 		//	}
-		
-		onNavBack: function(oEvent){
+
+		onNavBack: function (oEvent) {
 			this.getRouter().navTo("RouteView1");
 		},
-		
-		onToggleButtonPress: function(oEvent){
+
+		onToggleButtonPress: function (oEvent) {
 			var oViewModel = this.getView().getModel("viewModel");
-			
-			if(oViewModel.getProperty("/test") === "Data"){
+
+			if (oViewModel.getProperty("/test") === "Data") {
 				oViewModel.setProperty("/test", "");
-			}
-			else
+			} else
 				oViewModel.setProperty("/test", "Data");
+		},
+
+		_initMessagePopover: function () {
+			this.oMessagePopover = new sap.m.MessagePopover({
+				items: {
+					path: "message>/",
+					template: new sap.m.MessageItem({
+						title: "{message>message}",
+						subtitle: "{message>additionalText}",
+						groupName: {
+							parts: [{
+								path: 'message>controlIds'
+							}],
+							formatter: this.getGroupName
+						},
+						activeTitle: {
+							parts: [{
+								path: 'message>controlIds'
+							}],
+							formatter: this.isPositionable
+						},
+						type: "{message>type}",
+						description: "{message>message}"
+					})
+				}
+			});
+		},
+
+		_initMessageModel: function () {
+			var oMessageManager = sap.ui.getCore().getMessageManager();
+			this.getView().setModel(oMessageManager.getMessageModel(), "message");
+		},
+
+		onMessagePopoverPress: function (oEvent) {
+			this.getView().addDependent(this.oMessagePopover);
+			this.oMessagePopover.openBy(oEvent.getSource());
+		},
+
+		productListFactory: function (sId, oContext) {
+			var sDescription = oContext.getProperty("Description"),
+				sName = oContext.getProperty("Name"),
+				sStatus = oContext.getProperty("Status");
+				
+			if( sStatus !== "Available" ){
+				return new sap.m.StandardListItem({
+					title: sName,
+					description: sStatus
+				}).addStyleClass("redBackground");
+			}
+			else{
+				return new sap.m.StandardListItem({
+					title: sName,
+					description: sDescription
+				});
+			}
 		}
 
 	});
